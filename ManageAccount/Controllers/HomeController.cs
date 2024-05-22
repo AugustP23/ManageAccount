79d94d0380bc743d7af915d09aee18ae10087b9e
AugustP <phongbigguy23@gmail.com>
2024-05-22 22:52:42 +0700

    Add project files.

---
+++ ManageAccount/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ManageAccount.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ManageAccount.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ManageAccount/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManageAccount.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
